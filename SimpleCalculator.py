a83852dd2dcc1394ba12940ce9741ffa84fa0fbd
Surya.J <113592992+Suryaa2022@users.noreply.github.com>
2023-06-28 15:11:15 +0530

    SimpleCalculator.py

---
+++ SimpleCalculator.py
@@ -0,0 +1,45 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Division by zero"
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+while True:
+    choice = input("Enter choice (1-4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            result = add(num1, num2)
+            operation = '+'
+        elif choice == '2':
+            result = subtract(num1, num2)
+            operation = '-'
+        elif choice == '3':
+            result = multiply(num1, num2)
+            operation = '*'
+        else:
+            result = divide(num1, num2)
+            operation = '/'
+
+        print(f"{num1} {operation} {num2} = {result}")
+        break
+    else:
+        print("Invalid input. Please enter a valid choice.")
